3434d0540617aa80e49fb620f933c6989ecc1651
davidDevelopment <47590465+davidDevelopment@users.noreply.github.com>
2019-04-26 14:56:06 +0200

    Merge branch 'master' of https://github.com/davidDevelopment/Integration1
    # Conflicts:
    #	app/src/main/java/com/daviddev/projetgocaching_intgration1/Geocache.java

--- app/src/main/java/com/daviddev/projetgocaching_intgration1/DataBaseManager.java
+++ app/src/main/java/com/daviddev/projetgocaching_intgration1/DataBaseManager.java
@@ -5,6 +5,8 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
 import android.util.Log;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 
 public class DataBaseManager extends SQLiteOpenHelper implements BaseColumns{
@@ -15,7 +17,8 @@ public class DataBaseManager extends SQLiteOpenHelper implements BaseColumns{
     public static final String TABLE_NAME_GEOCACHE = "geocache";
     public static final String KEY_ID = "GeocacheID";
     public static final String COORD_X = "CoordX";
-    public static final String COORD_Y = "COORDY";
+    public static final String COORD_Y = "CoordY";
+    static String selectQuery;
 
     private static final String SQL_CREATE_ENTRIES =
             "CREATE TABLE IF NOT EXISTS " + TABLE_NAME_GEOCACHE + "("
@@ -64,7 +67,7 @@ public class DataBaseManager extends SQLiteOpenHelper implements BaseColumns{
         DataBaseManager dbHelper = new DataBaseManager(context);
         ArrayList<String> ArrayList = new ArrayList<String>();
         String name="";
-        String selectQuery = "SELECT "+COORD_X+" FROM " + TABLE_NAME_GEOCACHE+" WHERE "+KEY_ID+" = " +IDGeocache+" ";
+        selectQuery = "SELECT "+COORD_X+" FROM " + TABLE_NAME_GEOCACHE+" WHERE "+KEY_ID+" = " +IDGeocache+" ";
         SQLiteDatabase db = dbHelper.getReadableDatabase();
         Cursor c = db.rawQuery(selectQuery, null);
         // looping through all rows and adding to list
@@ -80,51 +83,33 @@ public class DataBaseManager extends SQLiteOpenHelper implements BaseColumns{
 
     }
 
-
-
     public static Coordinates getCoordinates(Context context, int IDGeocache){
 
-
-
-
         DataBaseManager dbHelper = new DataBaseManager(context);
-        ArrayList<String> ArrayList = new ArrayList<String>();
-        String name="";
-        String selectQuery = "SELECT "+COORD_X+" FROM " + TABLE_NAME_GEOCACHE+" WHERE "+KEY_ID+" = " +IDGeocache+" ";
         SQLiteDatabase db = dbHelper.getReadableDatabase();
-        Cursor c = db.rawQuery(selectQuery, null);
-        // looping through all rows and adding to list
-        if (c.moveToFirst()) {
-            do {
-                name = c.getString(c.getColumnIndex(COORD_X));
-                ArrayList.add(name);
-            } while (c.moveToNext());
-            Log.d("array", ArrayList.toString());
+        String selectQuery;
+        Cursor cursor;
+        double latitude = 0, longitude = 0;
+
+        selectQuery = "SELECT "+COORD_X+" FROM " + TABLE_NAME_GEOCACHE+" WHERE "+KEY_ID+" = " +IDGeocache+" ";
+        cursor = db.rawQuery(selectQuery, null);
+
+        if (cursor.moveToFirst()) {
+                latitude = Double.parseDouble(cursor.getString(cursor.getColumnIndex(COORD_X)));
         }
-        String a = ArrayList.get(0);
 
+        selectQuery = "SELECT " + COORD_Y +" FROM " + TABLE_NAME_GEOCACHE +" WHERE " + KEY_ID + " = " + IDGeocache+" ";
+        cursor = db.rawQuery(selectQuery, null);
 
-        DataBaseManager dbHelper2 = new DataBaseManager(context);
-        ArrayList<String> ArrayList2 = new ArrayList<String>();
-        String name2="";
-        String selectQuery2 = "SELECT "+COORD_Y+" FROM " + TABLE_NAME_GEOCACHE+" WHERE "+KEY_ID+" = " +IDGeocache+" ";
-        SQLiteDatabase db2 = dbHelper2.getReadableDatabase();
-        Cursor c2 = db.rawQuery(selectQuery, null);
-        // looping through all rows and adding to list
-        if (c2.moveToFirst()) {
-            do {
-                name2 = c.getString(c2.getColumnIndex(COORD_Y));
-                ArrayList2.add(name2);
-            } while (c2.moveToNext());
-            Log.d("array", ArrayList2.toString());
+        if (cursor.moveToFirst()) {
+            longitude = Double.parseDouble(cursor.getString(cursor.getColumnIndex(COORD_Y)));
         }
-        String b = ArrayList2.get(0);
 
-        double latitude = Double.parseDouble(a); //<--Mettre les coords récupéres dans la database ici
-        double longitude = Double.parseDouble(a);//<-- et ici
         Coordinates coords = new Coordinates(latitude, longitude);
         return coords;
     }
+
+
 
     public void getCoordY(){
 
